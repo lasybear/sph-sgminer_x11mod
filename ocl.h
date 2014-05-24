0d0d120ffd88e69814bd068c02be60f7068f0fa2
lasybear <beat2842@gmail.com>
2014-05-24 15:24:40 +0400

    initial

---
+++ ocl.h
@@ -0,0 +1,51 @@
+#ifndef __OCL_H__
+#define __OCL_H__
+
+#include "config.h"
+
+#include <stdbool.h>
+#ifdef __APPLE_CC__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+#include "miner.h"
+
+typedef struct {
+	cl_context context;
+	cl_kernel kernel;
+	cl_kernel kernel_blake;
+	cl_kernel kernel_bmw;
+	cl_kernel kernel_groestl;
+	cl_kernel kernel_skein;
+	cl_kernel kernel_jh;
+	cl_kernel kernel_keccak;
+	cl_kernel kernel_luffa;
+	cl_kernel kernel_cubehash;
+	cl_kernel kernel_shavite;
+	cl_kernel kernel_simd;
+	cl_kernel kernel_echo;
+	cl_command_queue commandQueue;
+	cl_program program;
+	cl_mem outputBuffer;
+	cl_mem CLbuffer0;
+	cl_mem padbuffer8;
+	cl_mem hash_buffer;
+	size_t padbufsize;
+	unsigned char cldata[80];
+	bool hasBitAlign;
+	bool hasOpenCL11plus;
+	bool hasOpenCL12plus;
+	bool goffset;
+	cl_uint vwidth;
+	size_t max_work_size;
+	size_t wsize;
+	size_t compute_shaders;
+	enum cl_kernels chosen_kernel;
+} _clState;
+
+extern char *file_contents(const char *filename, int *length);
+extern int clDevicesNum(void);
+extern _clState *initCl(unsigned int gpu, char *name, size_t nameSize);
+#endif /* __OCL_H__ */
